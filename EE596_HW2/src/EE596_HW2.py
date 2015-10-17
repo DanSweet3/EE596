b581b43454941bb88e4e2ccdfd9bddcf4a65302e
DanSweet3 <dan.sweet3@gmail.com>
2015-10-17 00:16:10 -0700

    Lots of work done today. I think I have most of the collection and prep of the training data, having problems with getting this data into the model.train() function.

--- EE596_HW2/src/EE596_HW2.py
+++ EE596_HW2/src/EE596_HW2.py
@@ -7,54 +7,172 @@ EE596 Homework 2
 '''
 import cv2
 import numpy as np
+import os
+from numpy import float32
 
-BACKGROUND_COLOR = (0,0,0)
+max_iter = 10           #number of iterations to use in the K-means algorithm
+epsilon = 1.0           #epsilon for accuracy of K-means algorithm
+K = 5                   #Number of K-means clusters
+
+IMG_TRAIN_PATH = '../face_training/'
+IMG_TRAIN_GT_PATH = '../face_training_groundtruth/'
+
+img_train = []
+img_train_gt = []
+ivec_train = []
+fvec_train = []
+center_train = []
+class_train = []
+
+kmean_img = []
+
+#Read all training images into list
+for filename in os.listdir(IMG_TRAIN_PATH):
+    img = cv2.imread(os.path.join(IMG_TRAIN_PATH, filename))
+    if img is not None:
+        img_train.append(img)
+
+#Read all training groundtruth images into list
+for filename in os.listdir(IMG_TRAIN_GT_PATH):
+    img = cv2.imread(os.path.join(IMG_TRAIN_GT_PATH, filename))
+    if img is not None:
+        img_thresh =cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_train_gt.append(img_thresh)
+
+#Reshape all images  
+for x in img_train:
+    ivec_train.append(x.reshape((-1,3)))
+
+#Convert to float for K-means
+for y in ivec_train:
+    fvec_train.append(np.float32(y))
+
+#Create K-Means Criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, max_iter, epsilon)
+
+#Perform K-Means on all training images
+pixel_valkmean_img = []
+i = 0
+for fvec in fvec_train:
+    ret, labels, centers = cv2.kmeans(fvec, K, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)     #Get K-Means
+    #res = labels.reshape((img_train[i].shape))
+    cent = np.uint8(centers)                                                                #Convert centers from float to uint8
+    #center_train.append(cent)                                                               #Save Centers for later during training
+    res = cent[labels.flatten()]                                                            #Create new image based on center labels
+    res2 = res.reshape((img_train[i].shape))                                                #Reshape to original image shape
+    
+    xyz = labels.flatten()
+    xyz2 = xyz.reshape((img_train[i].shape[0],img_train[i].shape[1]))
+
+    for c in centers:
+        center_train.append(c)
+#     cv2.imshow('Image: '+str(i),res2)
+#     cv2.waitKey()
+    i += 1
+    
+    kmean_img.append(xyz2)
+    
+# cv2.destroyAllWindows()
+
+# 10 if x == 0 else 0 for x in kmean_img[0]
+# Set up for Classifying
+# for k in range(0,K):
+#     abc = kmean_img[0] & img_train_gt[0]
+#     cv2.imshow('overlap',abc)
+#     cv2.waitKey()
+
+bayes_model = cv2.NormalBayesClassifier()
+
+for i in range(len(kmean_img)):
+    # Count how many of each cluster are in image
+    region_count = [0]*K
+    overlap_count = [0]*K
+    class_label = [0]*K
+    for x in range(kmean_img[i].shape[0]):
+        for y in range(kmean_img[i].shape[1]):
+            region_count[kmean_img[i][x,y]] += 1
+            if (kmean_img[i][x,y] & img_train_gt[i][x,y]):
+                overlap_count[kmean_img[i][x,y]] += 1
+    #Check Strength of match:
+    a = np.array(overlap_count, dtype = np.float)
+    b = np.array(region_count, dtype = np.float)
+    ratio = a/b
+    
+    x = 0
+    for element in ratio:
+        if (element >= 0.50):
+            class_label[x] = 1
+            class_train.append(1)
+        else:
+            class_label[x] = 0
+            class_train.append(0)
+        x+=1
+    
+    #class_train.append(class_label)    
+    
+#     print "Image Number: " + str(i+1)
+#     print overlap_count
+#     print region_count
+#     print ratio
+#     print class_label
 
 
-
-
-# #Function to process a passed image with a given binary threshold, close size and open size.
-# # Performs the following:
-# #  1. Performs binary threshold given image with passed binary_threshold parameter
-# #  2. Closes the image with passed close_size parameter. This filters small black dots out of each major organ
-# #  3. Opens the image with passed open_size parameter. This leaves only large blocks (organs) visible
-# #  4. Finds contours of image, keeping track of hierarchy to not lose nested contours. 
+# fjdaio = np.asarray(center_train)
+# print center_train
+# a1 = np.asarray(center_train)
+# print a1
+# a2 = np.float32(center_train)
+# print a2
+# b1 = np.asarray(class_train)
+# b2 = b1.astype(int)
 # 
-# def ProcessImage(image, binary_threshold, close_size, open_size):
-#     _, img_thresh = cv2.threshold(image, binary_threshold, 250, cv2.THRESH_BINARY)    #Perform binary threshold to get black and white
-#     cv2.imshow('GRAY', image)                                            #Show Grayscale
-#     cv2.imshow('THRESHOLD', img_thresh)                                  #Show binary threshold image
-#     
-#     # MORPHING
-#     # First close the image to get rid of black dots within larger shapes
-#     circ_mask = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(close_size,close_size))  
-#     img_morph = cv2.morphologyEx(img_thresh, cv2.MORPH_CLOSE, circ_mask)     
-#     cv2.imshow('CLOSED', img_morph)    
-#     
-#     # Then open to get rid of white elements that are too small to be significant
-#     circ_mask = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(open_size,open_size))
-#     img_morph = cv2.morphologyEx(img_morph, cv2.MORPH_OPEN, circ_mask)    
-#     cv2.imshow('OPENED', img_morph)
-#        
-#     #CONTOURS
-#     img_contours = np.zeros((image.shape[0], image.shape[1],3), np.uint8)     #Create empty image
-#     #Get contours, with hierarchy so I don't lose nested (child) contours
-#     contours, heirarchy = cv2.findContours(img_morph, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE) #Get contours
-#     #Add each contour with a random color 
-#     for contour_idx in range(len(contours)):
-#         #if not a child contour, draw with random color
-#         if heirarchy[0][contour_idx,3] == -1:
-#             cv2.drawContours(img_contours, contours, contour_idx, np.random.randint(0,255,3), -1)
-#         #if a child contour, just draw as background color
-#         else:
-#             cv2.drawContours(img_contours, contours, contour_idx, BACKGROUND_COLOR, -1)
-#     #Show contoured/colored output
-#     cv2.imshow('CONTOURS', img_contours)                                    
-#     cv2.waitKey(0)
-#     
-#     return
+# flattened = [val for sublist in a2 for val in sublist]
+# # print flattened
+# qwerty = np.array(a2)
+# asdf = np.array([0.0,0.0,0.0])
+# b2 = np.array(class_train)
+#print qwerty
+
+bayes_model.train(np.array(np.float32(center_train)),cv2.CV_ROW_SAMPLE,np.array(class_train))
+
+
+
+
+# for element in ratio:
+#     if()
+
+# overlap_count = []
 # 
-# #Call Process Image on the three pictures, with slightly different parameters for each
-# ProcessImage(cv2.imread('A:/UW/EE596/kidney.png', 0), 130, 3, 12);
-# ProcessImage(cv2.imread('A:/UW/EE596/e030.png', 0),   132, 3, 14);
-# ProcessImage(cv2.imread('A:/UW/EE596/g006.png', 0),   130, 2, 10);
+# for pixel in kmean_img[0]:
+# #    if kmean_img[0][pixel] & img_train_gt[0][pixel]:
+#         area_count[kmean_img[0][pixel]] = area_count[kmean_img[0][pixel]] + 1
+#         
+
+# cv2.imshow('ackvjla', img_train_gt[0])
+
+
+
+
+
+# cv2.imshow('overlap',kmean_img[0])
+# cv2.imshow('overlap',abc)
+# cv2.waitKey()
+cv2.destroyAllWindows()
+
+#ret, labels, centers = cv2.kmeans(fvec_train[0], K, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+#labels.reshape(100,100)
+# xyz = labels.reshape(img_train[1].shape[0],img_train[1].shape[1])
+# np.uint8(xyz)
+
+# cent = np.uint8(centers)
+# res = cent[labels.flatten()]
+# res2 = res.reshape((img_train[0].shape))
+# cv2.imshow('res2',res2)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+
+x = 0
+
+
+
